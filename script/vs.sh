955281de5dc5b3346a907716cdc90b345b4a3085
TG-99 <tempgh99@proton.me>
2024-04-15 19:27:26 +0600

    WebUI Update

--- script/vs.sh
+++ script/vs.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-wget https://github.com/coder/code-server/releases/download/v4.20.0/code-server-4.20.0-linux-amd64.tar.gz
-tar -xzvf code-server-4.20.0-linux-amd64.tar.gz
-cp -r code-server-4.20.0-linux-amd64 /usr/lib/code-server
+wget https://github.com/coder/code-server/releases/download/v4.23.0/code-server-4.23.0-linux-amd64.tar.gz
+tar -xzvf code-server-4.23.0-linux-amd64.tar.gz
+cp -r code-server-4.23.0-linux-amd64 /usr/lib/code-server
 ln -s /usr/lib/code-server/bin/code-server /bin/code-server
-rm -r code-server-4.20.0-linux-amd64*
+rm -r code-server-4.23.0-linux-amd64*
 
 while :
 do
