c3345a998fc59a1dd9a5686ebe455536e5125c29
TG-99 <tempgh99@proton.me>
2025-12-19 23:45:31 +0600

    WebUI:V3

--- script/nginx.sh
+++ script/nginx.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 mkdir --parents /home/
 cp web/index.html /var/www/html/index.html
-cat web/nginx_config | sed "s|8080 default_server;|$PORT default_server;|g" > /etc/nginx/sites-enabled/default
+cat web/nginx_config \
+| sed "s|8080 default_server;|${PORT} default_server;|g" \
+| sed "s|server_name _;|server_name ${BASE_URL};|g" \
+> /etc/nginx/sites-enabled/default
+
 /usr/sbin/nginx -g 'daemon off;'
--- script/vs.sh
+++ script/vs.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-wget https://github.com/coder/code-server/releases/download/v4.100.3/code-server-4.100.3-linux-amd64.tar.gz
-tar -xzvf code-server-4.100.3-linux-amd64.tar.gz
-cp -r code-server-4.100.3-linux-amd64 /usr/lib/code-server
+wget https://github.com/coder/code-server/releases/download/v4.107.0/code-server-4.107.0-linux-amd64.tar.gz
+tar -xzvf code-server-4.107.0-linux-amd64.tar.gz
+cp -r code-server-4.107.0-linux-amd64 /usr/lib/code-server
 ln -s /usr/lib/code-server/bin/code-server /bin/code-server
-rm -r code-server-4.100.3-linux-amd64*
+rm -r code-server-4.107.0-linux-amd64*
 
 while :
 do
